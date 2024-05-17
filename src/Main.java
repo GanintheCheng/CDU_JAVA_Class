ab6e02671827efcd6d2991ac890b6285aeb1de9d
GanintheCheng <ganzaicheng@qq.com>
2024-05-17 20:25:38 +0800

    期末代码1.0

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import System.StudentManagementSystem;
+import system.StudentManagementSystem;
 
 public class Main {
     public static void main(String[] args) {
--- src/system/StudentManagementSystem.java
+++ src/system/StudentManagementSystem.java
@@ -1,4 +1,4 @@
-package System;
+package system;
 
 import module.Student;
 
